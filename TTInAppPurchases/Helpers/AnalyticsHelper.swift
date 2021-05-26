1aca72512ea2d05455af8c6000aaa41b9132fe85
sandesh-naik <sandesh@indianrails.com>
2021-05-26 22:13:19 +0530

    Added subscription to app

--- TTInAppPurchases/Helpers/AnalyticsHelper.swift
+++ TTInAppPurchases/Helpers/AnalyticsHelper.swift
@@ -21,19 +21,27 @@ public class AnalyticsHelper {
     public static let shared = AnalyticsHelper()
     lazy var amplitudeInstance = Amplitude.instance()
     lazy var mixpanelInstance = Mixpanel.mainInstance()
+    
+    //TODO: - Temporary code to block logging for for document scanner app
+    var shouldLogEvent: Bool {
+        return Bundle.main.bundleIdentifier == "com.triviatribe.callrecorder"
+    }
 
     private init() {
+        if !shouldLogEvent { return }
         amplitudeInstance.trackingSessionEvents = true
     }
     
     // MARK: - Properties
     
     public func createAlias(_ userId: String) {
+        if !shouldLogEvent { return }
         amplitudeInstance.setUserId(userId, startNewSession: false)
         mixpanelInstance.createAlias(userId, distinctId: mixpanelInstance.distinctId)
     }
     
     public func setUserId(_ userId: String) {
+        if !shouldLogEvent { return }
         amplitudeInstance.setUserId(userId, startNewSession: false)
         mixpanelInstance.identify(distinctId: userId)
 //        AppsFlyerLib.shared().customerUserID = userId
@@ -41,18 +49,21 @@ public class AnalyticsHelper {
 
     // MARK: - Event Logging
     public func logEvent(_ event: String) {
+        if !shouldLogEvent { return }
         amplitudeInstance.logEvent(event)
         mixpanelInstance.track(event: event)
 //        AppsFlyerHelper.shared.logEvent(event: event, properties: nil)
     }
     
     public func logEvent(_ type: Constants.AnalyticsEvent) {
+        if !shouldLogEvent { return }
         amplitudeInstance.logEvent(type.rawValue)
         mixpanelInstance.track(event: type.rawValue)
 //        AppsFlyerHelper.shared.logEvent(event: type.rawValue, properties: nil)
     }
     
     public func logEvent(_ type: Constants.AnalyticsEvent, properties: [Constants.AnalyticsEventProperties: Any]) {
+        if !shouldLogEvent { return }
         amplitudeInstance.logEvent(type.rawValue, withEventProperties: properties)
         let mixpanelProperties = properties.reduce([:]) { (propertiesSoFar, arg1) -> [String: MixpanelType] in
             let (key, value) = arg1
@@ -66,11 +77,13 @@ public class AnalyticsHelper {
     }
     
     public func logRevenue(_ revenue: AMPRevenue) {
+        if !shouldLogEvent { return }
         amplitudeInstance.logRevenueV2(revenue)
 //        mixpanelInstance.people.trackCharge(amount: revenue.price.doubleValue)
     }
     
     public func updateUserProperties() {
+        if !shouldLogEvent { return }
         guard let userProperties = UserDefaults.standard.value(forKey: Constants.CallRecorderDefaults.userPropertiesKey) as? [String: Any] else {
             return
         }
